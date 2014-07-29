0f4f8bad1e312538aac4c1bd062d9fdaca891970
mb_kondou <kondou@maboroshi.biz>
2014-07-29 11:35:27 +0900

    update 14/7/29

--- test/js/jquery.textEllipsis.js
+++ test/js/jquery.textEllipsis.js
@@ -6,42 +6,96 @@
 
 @class 文字数制限プラグイン
 */
-
 $.fn.ellipsis = function(op){
   /**
   * optionです。
   * - afterText {String} : 語尾に追加される文字
-  * - limit {Int} : 何文字制御にするか
+  * - lines {Int} : 何行制御にするか
   * @method option
   */
   op = $.extend({
     afterText : "..."
   },op);
 
-  return this.each(function(e){
-    var $this;
-    $this = $(this);
+  return this.each(function(){
+    var $this = $(this);
 
-    var i,
-        text = $this.text().trim(),
-        textNum = text.length,
-        textCutNum = textNum,
-        afterTextNum = op.afterText.length;
+    ({
+      init : function(){
+        var text = $this.html(),
+            textNum = text.length,
+            textCutNum = textNum,
+            afterTextNum = op.afterText.length,
+            lower, upper,
+            line_height = this.lineHeight() * op.lines,
+            bite_size,
+            bite;
 
-    if(op.limit < textNum){
-      for(i = 0;i < textNum;i++){
-        textCut = text.substr(0,textCutNum-1);
-
-        //指定文字数に達したらループを停止
-        if(op.limit == textCutNum){
-          //末尾に足す分を引く
-          textCut = text.substr(0,textCutNum-afterTextNum);
-          break;
+        if($this.height() < this.lineHeight() * op.lines){
+          return false;
         }
 
-        textCutNum--;
-        $this.html(textCut+op.afterText);
+        lower = 0;
+        upper = textNum - 1;
+        while (lower <= upper) {
+          bite_size = lower + ((upper - lower) >> 1);
+
+          bite = this.textTrim(text, textNum - bite_size, op.afterText, 0);
+          $this.html(bite);
+
+          /* Check for overflow. */
+          if ($this.height() > line_height) {
+            upper = bite_size - 1;
+          }
+          else {
+            lower = bite_size + 1;
+          }
+        }
+      },
+
+      lineHeight : function(){
+        var floats = $this.css('float'),
+            position = $this.css('position'),
+            padding = $this.css('padding'),
+            html = $this.html(),
+            wrapper_id = 'line-height-test',
+            line_height;
+
+        $this.css('padding', 0);
+
+        if (floats !== 'none') {
+          $this.css('float', 'none');
+        }
+
+        if (position === 'absolute') {
+          $this.css('position', 'static');
+        }
+
+        /* Set the content to a small single character and wrap. */
+        $this.html('i').wrap('<div id="' + wrapper_id + '" />');
+
+        /* Calculate the line height by measuring the wrapper.*/
+        line_height = $('#' + wrapper_id).innerHeight();
+        $('#' + wrapper_id).find("p").append("ddd");
+
+        /* Remove the wrapper and reset the style/content. */
+        $this.html(html).css({
+          'padding': padding,
+          // 'float': floats,
+          'position': position
+        }).unwrap();
+
+        return line_height;
+      },
+
+      textTrim : function(str, bite_size, fill, roop){
+        var length = str.length;
+
+        if (bite_size === 0) {
+          return str;
+        }
+        return $.trim(str.substr(0, length - bite_size)) + fill;
       }
-    }
+    }).init();
   });
 }
